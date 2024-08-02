897e7dbd09133b70d5967ed8f2bbfa7b28f02d56
larnacaducktours <larnacaducktours@gmail.com>
2024-08-02 15:55:05 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('booking-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Booking form submitted! We will contact you soon.');
+});
